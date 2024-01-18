ef2dd22d3297214e169f1ffe3671cac60505554d
johnny <285552401@qq.com>
2024-01-18 12:05:27 +0800

    feat: init

---
+++ src/pages/login/components/Login.vue
@@ -0,0 +1,96 @@
+<template>
+  <t-form
+    ref="form"
+    :class="['item-container', `login-${type}`]"
+    :data="formData"
+    :rules="FORM_RULES"
+    label-width="0"
+    @submit="onSubmit"
+  >
+    <template v-if="type === 'password'">
+      <t-form-item name="account">
+        <t-input v-model="formData.account" size="large" placeholder="请输入用户名">
+          <template #prefix-icon>
+            <t-icon name="user" />
+          </template>
+        </t-input>
+      </t-form-item>
+
+      <t-form-item name="password">
+        <t-input
+          v-model="formData.password"
+          size="large"
+          :type="showPsw ? 'text' : 'password'"
+          clearable
+          placeholder="请输入密码"
+        >
+          <template #prefix-icon>
+            <t-icon name="lock-on" />
+          </template>
+          <template #suffix-icon>
+            <t-icon :name="showPsw ? 'browse' : 'browse-off'" @click="showPsw = !showPsw" />
+          </template>
+        </t-input>
+      </t-form-item>
+
+      <div class="check-container remember-pwd">
+        <t-checkbox>记住账号</t-checkbox>
+        <span class="tip">忘记密码？</span>
+      </div>
+    </template>
+
+    <t-form-item class="btn-container">
+      <t-button block size="large" type="submit">登录</t-button>
+    </t-form-item>
+  </t-form>
+</template>
+
+<script setup lang="tsx">
+import type { FormInstanceFunctions, FormRule, SubmitContext } from 'tdesign-vue-next';
+import { MessagePlugin } from 'tdesign-vue-next';
+import { useRoute, useRouter } from 'vue-router';
+
+const INITIAL_DATA = {
+  account: 'admin',
+  password: 'admin',
+  checked: false,
+};
+
+const FORM_RULES: Record<string, FormRule[]> = {
+  account: [{ required: true, type: 'error' }],
+  password: [{ required: true, type: 'error' }],
+};
+
+const type = ref('password');
+
+const form = ref<FormInstanceFunctions>();
+const formData = ref({ ...INITIAL_DATA });
+const showPsw = ref(false);
+
+const switchType = (val: string) => {
+  type.value = val;
+};
+
+const router = useRouter();
+const route = useRoute();
+
+const onSubmit = async (ctx: SubmitContext) => {
+  if (ctx.validateResult === true) {
+    try {
+      await userStore.login(formData.value);
+
+      MessagePlugin.success('登录成功');
+      const redirect = route.query.redirect as string;
+      const redirectUrl = redirect ? decodeURIComponent(redirect) : '/dashboard';
+      router.push(redirectUrl);
+    } catch (e) {
+      console.log(e);
+      MessagePlugin.error(e.message);
+    }
+  }
+};
+</script>
+
+<style lang="less" scoped>
+@import '../index.less';
+</style>
---
+++ src/pages/login/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="login-wrapper">
+    <div class="login-container">
+      <div class="title-container">
+        <h1 class="title margin-no">欢迎登录到</h1>
+        <h1 class="title">TTS</h1>
+        <div class="sub-title">
+          <p class="tip">{{ type === 'register' ? '已有账号?' : '没有账号吗?' }}</p>
+          <p class="tip" @click="switchType(type === 'register' ? 'login' : 'register')">
+            {{ type === 'register' ? '登录' : '注册新账号' }}
+          </p>
+        </div>
+      </div>
+
+      <login v-if="type === 'login'" />
+      <register v-else @register-success="switchType('login')" />
+    </div>
+
+    <footer class="copyright">Copyright @ 2023-{{ new Date().getFullYear() }} xx. All Rights Reserved</footer>
+  </div>
+</template>
+
+<script setup lang="tsx" name="LoginIndex">
+import Login from './components/Login.vue';
+import Register from './components/Register.vue';
+
+const type = ref('login');
+const switchType = (val: string) => {
+  type.value = val;
+};
+</script>
+
+<style lang="less" scoped>
+@import './index.less';
+</style>
